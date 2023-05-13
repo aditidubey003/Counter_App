1f4fff171c0b43fdc8235bf9ce0e2d7997f58c90
Aditi Dubey <aditidubey576@gmail.com>
2023-05-13 12:58:42 +0530

    completed the counter App

--- Counter App/script.js
+++ Counter App/script.js
@@ -1,12 +1,26 @@
 
-// Appraoch
+// Approach
 // 1. Create Elements
 // 2. Select elements 
-// 3. Add Event Listener on both buttons
-// 4. make a f that increments and decremnts and reset 
-// 5. call taht function
+// 3. Add Event Listener on all the buttons
+// 4. make functions that increments and decremnts and reset 
+// 5. call that function
 
 
+let counter =  document.querySelector('.box')
+let inc = document.querySelector('.inc')
+let dec = document.querySelector('.dec')
+let reset = document.querySelector('.reset')
 
+inc.addEventListener('click', function() {
+    counter.innerText = Number(counter.innerText) + 1;
+})
+
+dec.addEventListener('click', function() {
+    counter.innerText = Number(counter.innerText) - 1;
+})
+reset.addEventListener('click', function() {
+    counter.innerText = 0;
+})
 
 
