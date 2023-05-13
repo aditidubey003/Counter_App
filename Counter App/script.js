9af16cb394e050182c7df25951bf23ef2b033e20
Aditi Dubey <aditidubey576@gmail.com>
2023-05-13 11:49:49 +0530

    completed the UI

---
+++ Counter App/script.js
@@ -0,0 +1,12 @@
+
+// Appraoch
+// 1. Create Elements
+// 2. Select elements 
+// 3. Add Event Listener on both buttons
+// 4. make a f that increments and decremnts and reset 
+// 5. call taht function
+
+
+
+
+
